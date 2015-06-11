2771b30ae71cb0ba38a1550367fb3b656eb03340
David Wickström <davidwickstrom@gmail.com>
2015-06-11 16:45:11 +0200

    Add block, some xml and a template too

---
+++ src/app/code/community/KL/GuestWishlist/Block/Items.php
@@ -0,0 +1,6 @@
+<?php 
+
+class KL_GuestWishlist_Block_Items extends Mage_Catalog_Block_Category_View
+{
+    // TODO: set the collection somewhere
+}
---
+++ src/app/code/community/KL/GuestWishlist/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php 
+
+class KL_GuestWishlist_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'Hello Index!';
+//        $this->loadLayout();
+//        $this->renderLayout();
+    }
+}
