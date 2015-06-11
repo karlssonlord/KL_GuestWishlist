02ba3ba83e97b62c617a1f3e89f56aa68dcdd973
David Wickström <davidwickstrom@gmail.com>
2015-06-11 17:34:02 +0200

    Stash everything

--- src/app/code/community/KL/GuestWishlist/Block/Items.php
+++ src/app/code/community/KL/GuestWishlist/Block/Items.php
@@ -1,6 +1,6 @@
 <?php 
 
-class KL_GuestWishlist_Block_Items extends Mage_Catalog_Block_Category_View
+class KL_GuestWishlist_Block_Items extends Mage_Core_Block_Template
 {
     // TODO: set the collection somewhere
 }
--- src/app/code/community/KL/GuestWishlist/controllers/IndexController.php
+++ src/app/code/community/KL/GuestWishlist/controllers/IndexController.php
@@ -4,8 +4,7 @@ class KL_GuestWishlist_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo 'Hello Index!';
-//        $this->loadLayout();
-//        $this->renderLayout();
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
