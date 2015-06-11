e7e43d7279c9cf5d98035959a62160d35d465bd0
David Wickström <davidwickstrom@gmail.com>
2015-06-11 16:20:35 +0200

    Add layout and temple files

--- src/app/code/community/KL/GuestWishlist/controllers/ItemController.php
+++ src/app/code/community/KL/GuestWishlist/controllers/ItemController.php
@@ -4,7 +4,16 @@ class KL_GuestWishlist_ItemController extends Mage_Core_Controller_Front_Action
 {
     public function addAction()
     {
-        Mage::helper('guestwishlist')->saveFavourite($this->getRequest()->getPost('product_id'));
+        try
+        {
+            Mage::helper('guestwishlist')->saveFavourite($this->getRequest()->getPost('product_id'));
+        }
+        catch (Exception $e)
+        {
+            $this->getResponse()->setBody($this->__('Unable to add your favourite.'));
+            return $this->getResponse()->setHttpResponseCode(400);
+        }
+        $this->getResponse()->setHttpResponseCode(200);
     }
 
     public function removeAction()
@@ -15,7 +24,7 @@ class KL_GuestWishlist_ItemController extends Mage_Core_Controller_Front_Action
         }
         catch (Exception $e)
         {
-            $this->getResponse()->setBody($this->__('Unable to save your favourite.'));
+            $this->getResponse()->setBody($this->__('Unable to remove your favourite.'));
             return $this->getResponse()->setHttpResponseCode(400);
         }
 
